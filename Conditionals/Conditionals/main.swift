bc1f6bf494668799c44ccffda7e6b36cd7614074
Aayush Ranaut <aayush.ranaut@gmail.com>
2015-12-27 16:28:28 +0530

    Else if conditional

--- Conditionals/Conditionals/main.swift
+++ Conditionals/Conditionals/main.swift
@@ -3,7 +3,9 @@ let number = 11
 
 if number < 10 {
     print("Number is less than 10")
-}else
-{
+} else if number == 10 {
+    print("Number is 10")
+}
+else {
     print("Number is greater than 10")
 }
