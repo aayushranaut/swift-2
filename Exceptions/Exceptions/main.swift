a0bd2d69af79b47e1bd0ed675c052a1909c84bc4
Aayush Ranaut <aayush.ranaut@gmail.com>
2015-12-29 11:19:14 +0530

    A general catch exception

--- Exceptions/Exceptions/main.swift
+++ Exceptions/Exceptions/main.swift
@@ -1,22 +1,25 @@
 
 enum MyCalcError: ErrorType {
     case DivByZero
+    case DivByOne
 }
 
 func divide(firstValue first: Int, secondValue second: Int) throws -> Double {
     
     if second == 0 {
         throw MyCalcError.DivByZero
+    } else if second == 1 {
+        throw MyCalcError.DivByOne
     }
     
     return Double(first) / Double(second);
 }
 
 do {
-    var result = try divide(firstValue: 5, secondValue: 0)
+    var result = try divide(firstValue: 5, secondValue: 1)
     print("The result is \(result)")
 } catch MyCalcError.DivByZero {
     print("You can't divide by zero")
+} catch {
+    print("random catch every other exception")
 }
-
-
