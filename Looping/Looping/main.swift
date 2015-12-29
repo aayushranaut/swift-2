6befafe2109acd853e40dfee98017179e5d26a9f
Aayush Ranaut <aayush.ranaut@gmail.com>
2015-12-29 10:54:02 +0530

    Added for in loop

--- Looping/Looping/main.swift
+++ Looping/Looping/main.swift
@@ -1,8 +1,14 @@
 
-// For loop, for-in
+// For loop
 
 let numbers = [1, 2, 3, 4, 5]
 
 for var index = 0; index < numbers.count; index++ {
     print("The number is \(numbers[index])")
+}
+
+// for-in loop
+
+for num in numbers {
+    print("The number is \(num)")
 }
